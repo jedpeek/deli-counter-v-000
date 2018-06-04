fd9177439a6aa9e0e83ef25228fc923a7053ea57
Jed Peek <jedpeek@yahoo.com>
2018-06-04 21:15:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,12 @@
 # Write your code here.
 katz_deli = []
 def line(arr)
-  if arr.size == 0
-    puts "The line is currently empty."
-  end
 i = 0
 while i < arr.size
   current_line = []
 current_line = current_line.push("#{i + 1}. #{arr[i]}")
 i += 1
 end
-puts "The line is currently: #{current_line.to_s}"
+arr.size == 0 ? puts "The line is currently empty." : puts "The line is currently: #{current_line.to_s}"
+
 end
